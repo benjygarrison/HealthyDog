e8dc088315db68162d74dc4b07852e1a28981d02
benjyGarrison <benjygarrison@gmail.com>
2019-07-30 21:26:50 -0700

    Initial Commit

---
+++ HealthyDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthyDog
+//
+//  Created by Benjamin Garrison on 7/30/19.
+//  Copyright © 2019 Benji Garrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
