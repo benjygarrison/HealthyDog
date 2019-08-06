ac8bcc0d873d3b816771a10b493cbe983b69edd1
benjyGarrison <benjygarrison@gmail.com>
2019-08-06 13:19:25 -0700

    Button complete

--- HealthyDog/ViewController.swift
+++ HealthyDog/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController,
 UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var dogWeightPicker: UIPickerView!
-    
     var dogWeightPickerData1: [String] = [String]()
-    var dogWeightPickerData2: [String] = [String]()
-    var dogWeightPickerData3: [String] = [String]()
-    var dogWeightPickerData4: [String] = [String]()
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,50 +24,39 @@ UIPickerViewDelegate, UIPickerViewDataSource {
         self.dogWeightPicker.delegate = self
         self.dogWeightPicker.dataSource = self
         
-        let component1Array = Array(1...100)
+        let component1Array = Array(0...100)
         dogWeightPickerData1 = component1Array.map { String($0) }
-        dogWeightPickerData2 = ["point"]
-        let component3Array = Array(0...10)
-        dogWeightPickerData3 = component3Array.map { String($0) }
-        dogWeightPickerData4 = ["lbs", "kg"]
+
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 4
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if component == 0 {
-            return dogWeightPickerData1.count
-        }
-        if component == 1 {
-            return dogWeightPickerData2.count
-        }
-        if component == 2 {
-            return dogWeightPickerData3.count
-        }
-        return dogWeightPickerData4.count
+
+        return dogWeightPickerData1.count
     }
     
     func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if component == 0 {
-            return dogWeightPickerData1[row]
-        }
-        if component == 1  {
-            return dogWeightPickerData2[row]
-        }
-        if component == 2 {
-            return dogWeightPickerData3[row]
-        }
-        return dogWeightPickerData4[row]
+
+        return dogWeightPickerData1[row]
     }
     
     //func pickerView( _ pickerView: UIPickerView, //didSelectRow row: Int, inComponent component: //Int) {
     //}
+    
+    @IBAction func calculateButtonClicked(_ sender: Any) {
+        
+        print("yo")
+    }
+    
 }
 
